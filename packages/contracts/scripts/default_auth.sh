121d691b8fb59465956b27a0650acd47a25c0f02
Noy <59097122+noyyyy@users.noreply.github.com>
2024-01-01 17:36:17 +0800

    chore: add auth

--- packages/contracts/scripts/default_auth.sh
+++ packages/contracts/scripts/default_auth.sh
@@ -2,23 +2,23 @@
 set -euo pipefail
 pushd $(dirname "$0")/..
 
-export RPC_URL="http://localhost:5050";
+export RPC_URL="http://localhost:5050"
 
 export WORLD_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.world.address')
 
-export ACTIONS_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "autochessia::actions::actions" ).address')
+export HOME_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "autochessia::home::home" ).address')
 
 echo "---------------------------------------------------------------------------"
-echo world : $WORLD_ADDRESS 
+echo world : $WORLD_ADDRESS
 echo " "
-echo actions : $ACTIONS_ADDRESS
+echo home : $HOME_ADDRESS
 echo "---------------------------------------------------------------------------"
 
 # enable system -> component authorizations
-COMPONENTS=("Position" "Moves" )
+COMPONENTS=("Creature" "Player" "Piece" "InningBattle")
 
 for component in ${COMPONENTS[@]}; do
-    sozo auth writer $component $ACTIONS_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sozo auth writer $component $HOME_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
 done
 
 echo "Default authorizations have been successfully set."
