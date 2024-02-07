cd683e0d9b28752e5d9192d6b4b83d7db6609573
Noy <59097122+noyyyy@users.noreply.github.com>
2024-02-07 22:03:26 +0800

    style: lint

--- packages/client/.eslintrc.cjs
+++ packages/client/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-    env: { browser: true, es2020: true },
+    env: { node: true, browser: true, es2020: true },
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
